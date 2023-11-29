4b0c43b37ab2257210938c0ac61aa78ba68c6ea7
domgeomik <domgeomik@gmail.com>
2023-11-29 10:48:12 +0000

    Python_if_else_loops_functions

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-new = ""
-i = 0
-for c in str:
-if i != n:
-new += c
-i += 1
-return new
+if n < 0:
+return str
+else:
+str = str[0:n] + str[n+1:]
+return str
